734ef80246c91da48756b2512d3952a045f24ae2
oswystan <oswystan@126.com>
2016-07-25 13:39:57 +0800

    add gpio func; refactoring kmedia function
    Signed-off-by: oswystan <oswystan@126.com>

--- utils.sh
+++ utils.sh
@@ -16,15 +16,34 @@ function bk() {
     done
 }
 
+function akill() {
+    adb root
+    msg=`adb shell ps | grep $1`
+    echo $msg
+    echo $msg |cut -f 2 -d' '|xargs adb shell kill -9
+    sleep 0.2
+    msg=`adb shell ps | grep $1`
+    echo $msg
+}
+
 function kmedia() {
-    adb shell ps |grep mediaserver
-    adb shell ps |grep mediaserver |cut -f 6 -d' '|xargs adb shell kill -9
-    sleep 0.2 
-    adb shell ps |grep mediaserver
+    akill mediaserver
 }
 
 function logcat() {
     adb logcat -c && cls ; adb logcat 
+}
+
+function gpio() {
+    if [ $# -ne 3 ]; then
+        echo "usage: gpio <gpio_num direction value>"
+        return -1
+    fi
+    adb shell "echo $1 >/sys/class/gpio/export"
+    adb shell "echo $2 > /sys/class/gpio/gpio$1/direction"
+    adb shell "echo $3 > /sys/class/gpio/gpio$1/value"
+    adb shell "cat /sys/class/gpio/gpio$1/direction"
+    adb shell "cat /sys/class/gpio/gpio$1/value"
 }
 
 ##################################################################
