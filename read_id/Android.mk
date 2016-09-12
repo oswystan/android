b6ac17505b15329014c406667ef08808fa4c68ea
oswystan <oswystan@126.com>
2016-09-12 18:40:49 +0800

    add read_id
    Signed-off-by: oswystan <oswystan@126.com>

---
+++ read_id/Android.mk
@@ -0,0 +1,16 @@
+################################################
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= read_id.c
+
+LOCAL_SHARED_LIBRARIES := \
+
+LOCAL_C_INCLUDES += \
+
+LOCAL_C_INCLUDES += \
+
+LOCAL_MODULE:= read_id
+include $(BUILD_EXECUTABLE)
+
+################################################
---
+++ read_id/README.md
@@ -0,0 +1,1 @@
+this file is only used for read unique id of ov13870 
